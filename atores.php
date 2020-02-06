9d55d0de9209a071d1b9cffb0c2177387bb5df0e
Enzo Vaghetti <evaghetti@protonmail.com>
2020-02-06 15:13:41 -0300

    links na home e na listagem de atores

--- atores.php
+++ atores.php
@@ -4,5 +4,5 @@
     $resultado = mysqli_query($link, "SELECT * FROM Atores");
 
     while ($ator = mysqli_fetch_object($resultado))
-        echo "Ator $ator->id: $ator->nome<br>";
+        echo "Ator $ator->id: <a href='ator.php?id=$ator->id'>$ator->nome</a>   <a href='editar-ator.php?id=$ator->id'>Editar</a><br>";
 ?>
---
+++ index.php
@@ -0,0 +1,2 @@
+<a href="atores.php">Listagem de atores</a>
+<a href="criar-ator.php">Cadastrar ator</a>
